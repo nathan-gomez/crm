032186c5c0e379ae67adaba35af96e2fb0e3be9d
Fede Gomez <gomez.fede98@gmai.com>
2023-08-25 19:14:31 -0400

    db: added sessions table

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,17 @@
-CREATE TABLE public.users
+CREATE TABLE IF NOT EXISTS public.users
 (
     id uuid NOT NULL DEFAULT gen_random_uuid(),
-    name character varying(500) NOT NULL,
+    username varchar(500) NOT NULL UNIQUE,
     password character varying NOT NULL,
-    create_at timestamp with time zone NOT NULL DEFAULT date_trunc('seconds', NOW()),
+    role varchar(500) NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT date_trunc('seconds'::text, LOCALTIMESTAMP),
     updated_at timestamp with time zone,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.sessions (
+	id uuid NOT NULL DEFAULT gen_random_uuid(),
+	username varchar(500) NOT NULL,
+	created_at timestamp with time zone NOT NULL DEFAULT date_trunc('seconds'::text, LOCALTIMESTAMP),
     PRIMARY KEY (id)
 );
